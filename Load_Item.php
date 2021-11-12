fbb412feba4397054cf2513a6533c74a7a96960c
John Felix Miraña <85265640+John-Felix14@users.noreply.github.com>
2021-11-12 08:39:53 +0800

    Update Load_Item.php

--- Load_Item.php
+++ Load_Item.php
@@ -1,13 +1,6 @@
 <?php
 include 'DatabaseConfig.php';
 
-
-$HostName = "94phapp1";
-$DatabaseName = "mysql";
-$HostUser = "admin";
-$HostPass = "ChiefLanLord123";
-
-
 // Create connection
 $conn = new mysqli($HostName, $HostName, $HostName, $DatabaseName;
 
